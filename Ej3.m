1bc668edfa433281959475e40aefe4e5130a349b
AlfonsoFernandezSola <alfonso.fernandez.sola@alumnos.upm.es>
2025-10-08 13:18:15 +0200

    otra parte más hecha del ej3

--- Ej3.m
+++ Ej3.m
@@ -1,4 +1,4 @@
-%% 
+%% Funcion que calcula N decimales de e:
     
 function e = eCalc(N)
         e = 2;
@@ -20,4 +20,30 @@ function e = eCalc(N)
         end
 end
 
-fprintf('e = %.100f, err = %.10f', eCalc(100), exp(1));
+fprintf('\n\ne = %.100f, err = %.10f', eCalc(10), exp(1));
+
+%% Implemento la multiprecisión en la variable e:
+function e = eCalculo(N)
+        e = [2, (zeros(1, ceil(N/6)))];
+        A = ones(1, N);
+        for k = 1:N
+            r = 0;
+            q = 0;
+            for j = size(A, 2) + 1: -1: 2
+                i = j - 1;
+                r = mod(A(i)*10 + q, j);
+                q = floor((A(i) * 10 + q)/(j));
+                A(i) = r;
+            end
+            d = q;
+            %fprintf('Paso %.d : ', k);
+            %fprintf('%g ', A);
+            %fprintf(' ‐> digito out = %.d\n', d);
+            e = e + [0, zeros(1, floor((k - 1)/6)), d*10^(5 - mod(k - 1, 6)), zeros(1, ceil(N/6) - floor((k - 1)/6) - 1)];
+        end
+end
+
+E = eCalculo(10000);
+fprintf('\nel e calculado es: %d.', E(1));
+fprintf('%06g', E(2:end));
+fprintf('\ne = %.20f', exp(1));
